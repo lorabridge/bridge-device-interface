36a85614dcd8dda8a46d3e644ae1fcb2350f6806
tdam <tobias.dam@fhstp.ac.at>
2022-06-29 16:55:04 +0200

    modified navbar

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -10,27 +10,29 @@
 		Table,
 		TableDefaultRow
 	} from 'flowbite-svelte';
+
+  import logo from '$lib/assets/logo.webp';
 </script>
 
 <Navbar let:hidden let:toggle>
 	<NavBrand href="/">
 		<img
-			src="https://flowbite.com/docs/images/logo.svg"
+			src={logo}
 			class="mr-3 h-6 sm:h-9"
-			alt="Flowbite Logo"
+			alt="Lorabridge Logo"
 		/>
 		<span class="self-center whitespace-nowrap text-xl font-semibold dark:text-white">
-			Flowbite
+			Bride-Devices
 		</span>
 	</NavBrand>
-	<NavHamburger on:click={toggle} />
+	<!-- <NavHamburger on:click={toggle} />
 	<NavUl {hidden}>
 		<NavLi href="/" active={true}>Home</NavLi>
 		<NavLi href="/about">About</NavLi>
 		<NavLi href="/services">Services</NavLi>
 		<NavLi href="/pricing">Pricing</NavLi>
 		<NavLi href="/contact">Contact</NavLi>
-	</NavUl>
+	</NavUl> -->
 </Navbar>
 
 <slot />
