577b31ad7a747216cd8e60f66e32c908601342f3
tdam <tobias.dam@fhstp.ac.at>
2022-11-07 15:31:55 +0100

    fix for wrong topic

--- src/routes/stats/zigbee/join/+server.ts
+++ src/routes/stats/zigbee/join/+server.ts
@@ -1,7 +1,7 @@
 import { mClient } from '$lib/_mqtt';
 
 export async function POST({ params, request }) {
-    await mClient.publish("zigbee2mqtt/request/permit_join", JSON.stringify({ "value": true, "time": 300 }));
+    await mClient.publish("zigbee2mqtt/bridge/request/permit_join", JSON.stringify({ "value": true, "time": 300 }));
 
     console.log("Zigbee2Mqtt joining enabled");
     return new Response(undefined);
